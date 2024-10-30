1f22b1dce2b13a700a8ad0625055805be721acce
Kevin Chappell <kevin.b.chappell@gmail.com>
2024-10-30 10:43:26 -0700

    fix: es module output config

--- vite.config.lib.mjs
+++ vite.config.lib.mjs
@@ -1,15 +1,28 @@
 import { defineConfig } from 'vite'
+import banner from 'vite-plugin-banner'
 import { resolve } from 'path'
 
+import pkg from './package.json'
+
+const bannerTemplate = `
+/**
+${pkg.name} - ${pkg.homepage}
+Version: ${pkg.version}
+Author: ${pkg.author}
+*/
+`
+
 export default defineConfig({
+  root: './',
   build: {
     lib: {
       entry: resolve(__dirname, 'src/js/index.js'),
       name: 'formeo-languages',
       fileName: format => `formeo-languages.${format}.js`,
-      formats: ['es', 'umd'],
+      formats: ['es', 'cjs', 'umd'],
     },
     outDir: 'dist',
     emptyOutDir: true,
   },
+  plugins: [banner(bannerTemplate)],
 })
